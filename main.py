b67c73b78917af41875664600b124c741a8a8e2d
aaravkothari <aaravkothari155@gmail.com>
2025-06-26 12:38:44 -0400

    tracker set up

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     tracker = Tracker('models/best.pt')
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
 
     save_video(video_frames, 'output_videos/output_video.avi')
 
